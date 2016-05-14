c7b14b3452f36815b99eadc5b44ee90bf0a52293
Arckenver <arckenver@gmail.com>
2016-05-14 21:56:23 +0200

    Changed version

--- src/main/java/com/arckenver/nations/NationsPlugin.java
+++ src/main/java/com/arckenver/nations/NationsPlugin.java
@@ -96,7 +96,7 @@ import com.arckenver.nations.task.TaxesCollectRunnable;
 import com.google.common.collect.ImmutableMap;
 import com.google.inject.Inject;
 
-@Plugin(id = "com.arckenver.nations", name = "Nations", version = "0.6", description = "A towny-like worldguard-like zone managment plugin.")
+@Plugin(id = "com.arckenver.nations", name = "Nations", version = "0.7", description = "A towny-like worldguard-like zone managment plugin.")
 public class NationsPlugin
 {
 	private File rootDir;
