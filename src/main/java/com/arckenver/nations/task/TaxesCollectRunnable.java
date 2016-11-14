b61b377789cebbde060c9574e4608e23c828287a
Kah <t.sbrissa@gmail.com>
2016-11-14 17:46:27 +0100

    Clean file imports

--- src/main/java/com/arckenver/nations/task/TaxesCollectRunnable.java
+++ src/main/java/com/arckenver/nations/task/TaxesCollectRunnable.java
@@ -4,7 +4,6 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Optional;
 import java.util.UUID;
-import java.util.regex.Pattern;
 
 import org.spongepowered.api.Sponge;
 import org.spongepowered.api.service.economy.account.Account;
