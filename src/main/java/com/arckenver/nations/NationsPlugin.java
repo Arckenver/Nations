b5f9f11a1012fb7d444ecf61528d8a03d2742da5
Kah <t.sbrissa@gmail.com>
2017-07-16 12:46:38 +0200

    bump to 2.5

--- src/main/java/com/arckenver/nations/NationsPlugin.java
+++ src/main/java/com/arckenver/nations/NationsPlugin.java
@@ -113,7 +113,7 @@ import com.arckenver.nations.task.TaxesCollectRunnable;
 import com.google.common.collect.ImmutableMap;
 import com.google.inject.Inject;
 
-@Plugin(id = "nations", name = "Nations", version = "2.4", authors={"Arckenver"}, description = "A towny-like worldguard-like zone managment plugin.", url="https://github.com/Arckenver/Nations")
+@Plugin(id = "nations", name = "Nations", version = "2.5", authors={"Arckenver"}, description = "A towny-like worldguard-like zone managment plugin.", url="https://github.com/Arckenver/Nations")
 public class NationsPlugin
 {
 	private File rootDir;
