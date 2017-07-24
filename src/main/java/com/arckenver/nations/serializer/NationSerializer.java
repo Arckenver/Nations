a83ffc934c284b628e97cd79318a908bbfbe4cea
Kah <t.sbrissa@gmail.com>
2017-07-24 21:07:17 +0200

    Now saving citizens, ministers and president in admin nations

--- src/main/java/com/arckenver/nations/serializer/NationSerializer.java
+++ src/main/java/com/arckenver/nations/serializer/NationSerializer.java
@@ -24,21 +24,29 @@ public class NationSerializer implements JsonSerializer<Nation>
 	public JsonElement serialize(Nation nation, Type type, JsonSerializationContext ctx)
 	{
 		JsonObject json = new JsonObject();
-		
+
 		json.add("uuid", new JsonPrimitive(nation.getUUID().toString()));
 		json.add("name", new JsonPrimitive(nation.getRealName()));
 		json.add("admin", new JsonPrimitive(nation.isAdmin()));
+
+		if (!nation.isAdmin())
+		{
+			json.add("taxes", new JsonPrimitive(nation.getTaxes()));
+			json.add("extras", new JsonPrimitive(nation.getExtras()));
+			json.add("extraspawns", new JsonPrimitive(nation.getExtraSpawns()));
+
+		}
 		
 		if (nation.hasTag())
 			json.add("tag", new JsonPrimitive(nation.getTag()));
-		
+
 		JsonObject flags = new JsonObject();
 		for (Entry<String, Boolean> e : nation.getFlags().entrySet())
 		{
 			flags.add(e.getKey(), new JsonPrimitive(e.getValue()));
 		}
 		json.add("flags", flags);
-		
+
 		JsonObject perms = new JsonObject();
 		for (Entry<String, Hashtable<String, Boolean>> e : nation.getPerms().entrySet())
 		{
@@ -50,7 +58,7 @@ public class NationSerializer implements JsonSerializer<Nation>
 			perms.add(e.getKey(), obj);
 		}
 		json.add("perms", perms);
-		
+
 		JsonArray rectArray = new JsonArray();
 		for (Rect r : nation.getRegion().getRects())
 		{
@@ -63,16 +71,16 @@ public class NationSerializer implements JsonSerializer<Nation>
 			rectArray.add(rectJson);
 		}
 		json.add("rects", rectArray);
-		
+
 		JsonArray zonesArray = new JsonArray();
 		for (Zone zone : nation.getZones().values())
 		{
 			JsonObject zoneObj = new JsonObject();
-			
+
 			zoneObj.add("uuid", new JsonPrimitive(zone.getUUID().toString()));
 			if (zone.isNamed())
 				zoneObj.add("name", new JsonPrimitive(zone.getRealName()));
-			
+
 			JsonObject rectJson = new JsonObject();
 			rectJson.add("world", new JsonPrimitive(zone.getRect().getWorld().toString()));
 			rectJson.add("minX", new JsonPrimitive(zone.getRect().getMinX()));
@@ -80,26 +88,26 @@ public class NationSerializer implements JsonSerializer<Nation>
 			rectJson.add("minY", new JsonPrimitive(zone.getRect().getMinY()));
 			rectJson.add("maxY", new JsonPrimitive(zone.getRect().getMaxY()));
 			zoneObj.add("rect", rectJson);
-			
+
 			if (zone.getOwner() != null)
 			{
 				zoneObj.add("owner", new JsonPrimitive(zone.getOwner().toString()));
 			}
-			
+
 			JsonArray coownersArray = new JsonArray();
 			for (UUID coowner : zone.getCoowners())
 			{
 				coownersArray.add(new JsonPrimitive(coowner.toString()));
 			}
 			zoneObj.add("coowners", coownersArray);
-			
+
 			JsonObject zoneFlags = new JsonObject();
 			for (Entry<String, Boolean> e : zone.getFlags().entrySet())
 			{
 				zoneFlags.add(e.getKey(), new JsonPrimitive(e.getValue()));
 			}
 			zoneObj.add("flags", zoneFlags);
-			
+
 			JsonObject zonePerms = new JsonObject();
 			for (Entry<String, Hashtable<String, Boolean>> e : zone.getPerms().entrySet())
 			{
@@ -111,16 +119,16 @@ public class NationSerializer implements JsonSerializer<Nation>
 				zonePerms.add(e.getKey(), obj);
 			}
 			zoneObj.add("perms", zonePerms);
-			
+
 			if (zone.isForSale())
 			{
 				zoneObj.add("price", new JsonPrimitive(zone.getPrice()));
 			}
-			
+
 			zonesArray.add(zoneObj);
 		}
 		json.add("zones", zonesArray);
-		
+
 		JsonObject spawns = new JsonObject();
 		for (Entry<String, Location<World>> e : nation.getSpawns().entrySet())
 		{
@@ -129,34 +137,27 @@ public class NationSerializer implements JsonSerializer<Nation>
 			loc.add("x", new JsonPrimitive(e.getValue().getX()));
 			loc.add("y", new JsonPrimitive(e.getValue().getY()));
 			loc.add("z", new JsonPrimitive(e.getValue().getZ()));
-			
+
 			spawns.add(e.getKey(), loc);
 		}
 		json.add("spawns", spawns);
-		
-		if (!nation.isAdmin())
-		{
-			json.add("president", new JsonPrimitive(nation.getPresident().toString()));
-			
-			JsonArray ministersArray = new JsonArray();
-			for (UUID minister : nation.getMinisters())
-			{
-				ministersArray.add(new JsonPrimitive(minister.toString()));
-			}
-			json.add("ministers", ministersArray);
-			
-			JsonArray citizensArray = new JsonArray();
-			for (UUID citizen : nation.getCitizens())
-			{
-				citizensArray.add(new JsonPrimitive(citizen.toString()));
-			}
-			json.add("citizens", citizensArray);
 
-			json.add("taxes", new JsonPrimitive(nation.getTaxes()));
-			json.add("extras", new JsonPrimitive(nation.getExtras()));
-			json.add("extraspawns", new JsonPrimitive(nation.getExtraSpawns()));
-			
+		json.add("president", new JsonPrimitive(nation.getPresident().toString()));
+
+		JsonArray ministersArray = new JsonArray();
+		for (UUID minister : nation.getMinisters())
+		{
+			ministersArray.add(new JsonPrimitive(minister.toString()));
 		}
+		json.add("ministers", ministersArray);
+
+		JsonArray citizensArray = new JsonArray();
+		for (UUID citizen : nation.getCitizens())
+		{
+			citizensArray.add(new JsonPrimitive(citizen.toString()));
+		}
+		json.add("citizens", citizensArray);
+
 		return json;
 	}
 }
